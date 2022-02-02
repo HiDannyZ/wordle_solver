a13dc3e88e1a0400e9dded5e3ca6de18d774c7ac
Danny Zhu <chdannyzhu@yahoo.com>
2022-02-02 18:46:26 -0500

    Created more empty functions

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ import config
 
 target = "wrong"
 
-def wordle_solver():
-    with 
-
-
+def load_dict():
+    with open('dict.txt') as f:
+        words = f.read().splitlines()
+    return words
+    
 def pick_word(words):
     return words[0] if len(words) > 0 else ""
+
+def filter_words():
+    return
+
+# GREEN = "="
+# YELLOW = "+"
+# WRONG = "-"
+def match_word():
+    
         
-if __name__ == "__main_":
-    print(f"LOOOOOOOOOOOOOOOOOOOOOOOOOOOL")
+if __name__ == "__main__":
+    words = load_dict()
