89f2c1fff7a5111db7995d7f74293ffc9a0fbe05
Danny Zhu <chdannyzhu@yahoo.com>
2022-02-02 17:55:27 -0500

    First commit

---
+++ config.py
@@ -0,0 +1,8 @@
+import string
+
+ALLOWABLE_CHARACTERS = set(string.ascii_letters)
+ATTEMPTS = 6
+WORD_LENGTH = 5
+GREEN = "="
+YELLOW = "+"
+WRONG = "-"
---
+++ main.py
@@ -0,0 +1,13 @@
+import config
+
+target = "wrong"
+
+def wordle_solver():
+    with 
+
+
+def pick_word(words):
+    return words[0] if len(words) > 0 else ""
+        
+if __name__ == "__main_":
+    print(f"LOOOOOOOOOOOOOOOOOOOOOOOOOOOL")
